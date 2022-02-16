ff1462327d63514034c9d04984cd2e802b0fa0da
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-02-17 04:19:01 +0600

    error added

--- javascript/monthly.js
+++ javascript/monthly.js
@@ -1,10 +1,11 @@
 
 document.getElementById('calculate').addEventListener('click', function(){
     const incomeAmmount = getInputValue('income-ammount');
-    const foodCost = getInputValue('food-cost');
-    const houseCost = getInputValue('house-cost');
-    const dressCost = getInputValue('dress-cost');
-    const totalCost = foodCost + houseCost + dressCost;
+    // const foodCost = getInputValue('food-cost');
+    // const houseCost = getInputValue('house-cost');
+    // const dressCost = getInputValue('dress-cost');
+    // const totalCost = foodCost + houseCost + dressCost;
+    const totalCost = update();
     if(totalCost > incomeAmmount){
         alert("You can't spend more than you earn.");
     } else{
@@ -15,14 +16,23 @@ document.getElementById('calculate').addEventListener('click', function(){
 
 document.getElementById('save_button').addEventListener('click', function(){
     const income = getInputValue('income-ammount');
+    const totalCost = income - update()
     const saveParcentage = getInputValue('save-parcentage');
-    const saveMoney = income/saveParcentage;
+    const saveMoney = income*saveParcentage/100;
+    const lastMoney = totalCost - saveMoney
     document.getElementById('save-ammount').innerText = saveMoney;
+    document.getElementById('remaining-balance').innerText = lastMoney;
 })
 
+// function
+function update(){
+    const foodCost = getInputValue('food-cost');
+    const houseCost = getInputValue('house-cost');
+    const dressCost = getInputValue('dress-cost');
+    const total = foodCost + houseCost + dressCost;
+    return total;
+}
 
-
-// function 
 function getInputValue(inputId){
     const input = document.getElementById(inputId);
     const inputValue = parseInt(input.value);
